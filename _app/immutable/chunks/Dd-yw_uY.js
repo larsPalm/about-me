f1f1d817a8b4fbb4907a7e16d73714e9d76586ff
Lars Palm <larspalm97@gmail.com>
2026-05-25 00:55:27 +0200

    Updates

---
+++ _app/immutable/chunks/Dd-yw_uY.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_18ylmol?.base??"/about-me",a=globalThis.__sveltekit_18ylmol?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/entry/start.7cYq57NP.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CWKG7oS4.js";export{o as load_css,r as start};
