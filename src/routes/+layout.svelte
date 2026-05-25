22a4d9e4a7d82332482a04f3becdc191e3642fb6
Lars Palm <larspalm97@gmail.com>
2026-05-25 14:42:23 +0200

    la til personvernerklæring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-    import "../styles/global.scss";
-    import LangToggle from "../components/LangToggle.svelte";
-    import ThemeToggle from "../components/ThemeToggle.svelte";
+  import "../styles/global.scss";
+  import LangToggle from "../components/LangToggle.svelte";
+  import PrivacyModal from "../components/PrivacyModal.svelte";
+  import ThemeToggle from "../components/ThemeToggle.svelte";
 </script>
 
 <header class="toolbar">
-    <LangToggle />
-    <ThemeToggle />
+  <PrivacyModal />
+  <LangToggle />
+  <ThemeToggle />
 </header>
 
 <slot />
 
 <style>
-    .toolbar {
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        gap: 0.65rem;
-        width: min(1120px, calc(100% - 2rem));
-        margin: 0 auto;
-        padding-top: 1rem;
-    }
+  .toolbar {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 0.65rem;
+    width: min(1120px, calc(100% - 2rem));
+    margin: 0 auto;
+    padding-top: 1rem;
+  }
 </style>
