ef47bf28e33396e3df46cdea7cb21b68f637e9c0
Lars Palm <larspalm97@gmail.com>
2026-05-25 14:41:54 +0200

    Updates

---
+++ _app/immutable/chunks/B7R-slby.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bpebkg?.base??"/about-me",e=globalThis.__sveltekit_bpebkg?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/Dd-yw_uY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_18ylmol?.base??"/about-me",a=globalThis.__sveltekit_18ylmol?.assets??s??"";export{a,s as b};
